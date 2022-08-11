6530b54dd5ef58ca0fd49fa92a0c80adffd9810c
KetanGadwale <ketanvgadwale@gmail.com>
2022-08-11 17:34:21 +0530

    chore: add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Experts from './pages/Experts';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Luma</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/experts' element={<Experts />} />
+      </Routes>
     </div>
   );
 }
