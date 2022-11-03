4859fdd55d89c2c7ecc985852601124544b46fd4
KetanGadwale <ketanvgadwale@gmail.com>
2022-11-03 23:49:23 +0530

    fix: api routes fixed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -42,59 +42,60 @@ function Login() {
 export default Login;
 */
 
-import { Button, Form, Input } from "antd";
-import React from "react";
-import toast from "react-hot-toast";
-import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { hideLoading, showLoading } from "../redux/alertsSlice";
+import { Button, Form, Input } from 'antd';
+import React from 'react';
+import toast from 'react-hot-toast';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { hideLoading, showLoading } from '../redux/alertsSlice';
 
 function Login() {
- // const {loading} = useSelector(state => state.alert)
+  // const {loading} = useSelector(state => state.alert)
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const onFinish = async (values) => {
     try {
       dispatch(showLoading());
-      const response = await axios.post('http://localhost:5500/login',values);
-      console.log(response)
+      const response = await axios.post('/login', values);
+      console.log(response);
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
-        console.log(response.data.data)
-        localStorage.setItem("token", response.data.data);
-        navigate("/");
+        console.log(response.data.data);
+        localStorage.setItem('token', response.data.data);
+        navigate('/');
       } else {
         toast.error(response.data.message);
       }
     } catch (error) {
       dispatch(hideLoading());
-      toast.error("Something went wrong");
+      toast.error('Something went wrong');
     }
   };
 
   return (
-    <div className="authentication">
-      <div className="authentication-form card p-3">
-        <h1 className="card-title">Welcome Back</h1>
-        <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item label="Email" name="email">
-            <Input placeholder="Email" />
+    <div className='authentication'>
+      <div className='authentication-form card p-3'>
+        <h1 className='card-title'>Welcome Back</h1>
+        <Form layout='vertical' onFinish={onFinish}>
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='Email' />
           </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input placeholder="Password" type="password" />
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='Password' type='password' />
           </Form.Item>
 
-          
-          <Button className="primary-button my-2 full-width-button" htmlType="submit">
+          <Button
+            className='primary-button my-2 full-width-button'
+            htmlType='submit'
+          >
             LOGIN
           </Button>
 
-          <Link to="/register" className="anchor mt-2">
+          <Link to='/register' className='anchor mt-2'>
             CLICK HERE TO REGISTER
           </Link>
-         
         </Form>
       </div>
     </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -64,58 +64,57 @@ export default function Signup() {
       ); 
   }
 */
-import { Button, Form, Input } from "antd";
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import axios from "axios";
-import toast from "react-hot-toast";
-import { hideLoading, showLoading } from "../redux/alertsSlice";
+import { Button, Form, Input } from 'antd';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { hideLoading, showLoading } from '../redux/alertsSlice';
 
 function Register() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const onFinish = async(values) => {
+  const onFinish = async (values) => {
     try {
       dispatch(showLoading());
-      const response = await axios.post('http://localhost:5500/register',values);
+      const response = await axios.post('/register', values);
       dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
-        navigate("/login");
+        navigate('/login');
       } else {
         toast.error(response.data.message);
       }
     } catch (error) {
       dispatch(hideLoading());
-      toast.error("Something went wrong");
+      toast.error('Something went wrong');
     }
   };
 
   return (
-
-    <div className="authentication">
-      <div className="authentication-form card p-3">
-        <h1 className="card-title">Nice To Meet U</h1>
-        <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item label="Name" name="name">
-            <Input placeholder="Name" />
+    <div className='authentication'>
+      <div className='authentication-form card p-3'>
+        <h1 className='card-title'>Nice To Meet U</h1>
+        <Form layout='vertical' onFinish={onFinish}>
+          <Form.Item label='Name' name='name'>
+            <Input placeholder='Name' />
           </Form.Item>
-          <Form.Item label="Email" name="email">
-            <Input placeholder="Email" />
+          <Form.Item label='Email' name='email'>
+            <Input placeholder='Email' />
           </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input placeholder="Password" type="password" />
+          <Form.Item label='Password' name='password'>
+            <Input placeholder='Password' type='password' />
           </Form.Item>
 
           <Button
-            className="primary-button my-2 full-width-button"
-            htmlType="submit"
+            className='primary-button my-2 full-width-button'
+            htmlType='submit'
           >
             REGISTER
           </Button>
 
-          <Link to="/login" className="anchor mt-2">
+          <Link to='/login' className='anchor mt-2'>
             CLICK HERE TO LOGIN
           </Link>
         </Form>
