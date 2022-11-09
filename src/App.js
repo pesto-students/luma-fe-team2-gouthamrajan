dc2d535b22a65e5552acad5a8a321987e6d671e3
KetanGadwale <ketanvgadwale@gmail.com>
2022-11-09 10:56:41 +0530

    refactor: redux store

--- src/App.js
+++ src/App.js
@@ -1,83 +1,88 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useSelector } from "react-redux";
-import PaymentPage from "./payment/PaymentPage/PaymentPage"
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Home from './pages/Home'
-import ApplyExpert from "./pages/ApplyExperts";
-import { Button } from 'antd'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import PaymentPage from './payment/PaymentPage/PaymentPage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import ApplyExpert from './pages/ApplyExperts';
 import { Toaster } from 'react-hot-toast';
 import ProtectedRoute from './components/ProtectedRoute';
-import Notifications from "./pages/Notifications";
+import Notifications from './pages/Notifications';
 import PublicRoute from './components/PublicRoute';
 import Userslists from './pages/Admin/Userslists';
 import ExpertsList from './pages/Admin/ExpertsList';
-import Profile from "./pages/Doctor/Profile";
-import BookAppointment from "./pages/BookAppointment";
-import Appointments from "./pages/Appointments";
-import ExpertAppointments from "./pages/Doctor/ExpertAppointments";
-import { showLoading, hideLoading } from "./redux/alertsSlice";
+import Profile from './pages/Doctor/Profile';
+import BookAppointment from './pages/BookAppointment';
+import Appointments from './pages/Appointments';
+import ExpertAppointments from './pages/Doctor/ExpertAppointments';
+import { showLoading, hideLoading } from './redux/alertsSlice';
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
 
   return (
     <BrowserRouter>
-      {loading && <div className='spinner-parent'>
-        <div className="spinner-border" role="status">
-
+      {loading && (
+        <div className='spinner-parent'>
+          <div className='spinner-border' role='status'></div>
         </div>
-
-      </div>}
-      <Toaster position="top-center" reverseOrder={false} />
+      )}
+      <Toaster position='top-center' reverseOrder={false} />
       <Routes>
-        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-        <Route path="/register" element={<PublicRoute><Signup /></PublicRoute>} />
-        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route
-          path="/apply-Expert"
+          path='/login'
           element={
-
-            <ApplyExpert />
-
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
           }
         />
         <Route
-          path="/notifications"
+          path='/register'
           element={
-
-            <Notifications />
-
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
           }
         />
-     
-      <Route
-        path="/admin/userslist"
-        element={
-          <ProtectedRoute>
-            <Userslists />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/admin/expertslist"
-        element={
-          <ProtectedRoute>
-            <ExpertsList />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-          path="/expert/profile/:userId"
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='/apply-Expert' element={<ApplyExpert />} />
+        <Route path='/notifications' element={<Notifications />} />
+
+        <Route
+          path='/admin/userslist'
+          element={
+            <ProtectedRoute>
+              <Userslists />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/admin/expertslist'
+          element={
+            <ProtectedRoute>
+              <ExpertsList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/expert/profile/:userId'
           element={
             <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
         />
-      <Route
-          path="/book-appointment/:expertId"
+        <Route
+          path='/book-appointment/:expertId'
           element={
             <ProtectedRoute>
               <BookAppointment />
@@ -85,35 +90,33 @@ function App() {
           }
         />
         <Route
-          path="/appointments"
+          path='/appointments'
           element={
             <ProtectedRoute>
-                <Appointments />
+              <Appointments />
             </ProtectedRoute>
           }
         />
         <Route
-          path="/expert/appointments"
+          path='/expert/appointments'
           element={
             <ProtectedRoute>
-                <ExpertAppointments />
+              <ExpertAppointments />
             </ProtectedRoute>
           }
         />
-      
+
         <Route
           path='/payment'
           element={
             <ProtectedRoute>
-               <PaymentPage />
+              <PaymentPage />
             </ProtectedRoute>
           }
         />
-</Routes>
-
+      </Routes>
     </BrowserRouter>
   );
-
 }
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
-import { alertsSlice } from "./alertsSlice";
-import { userSlice } from "./userSlice";
-const rootReducer = combineReducers({
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { alertsSlice } from './alertsSlice';
+import { userSlice } from './userSlice';
+// const rootReducer = combineReducers({
+//     alerts: alertsSlice.reducer,
+//     user:userSlice.reducer
+//   });
+
+const store = configureStore({
+  reducer: {
     alerts: alertsSlice.reducer,
-    user:userSlice.reducer
-  });
-  
-  const store = configureStore({
-    reducer: rootReducer,
-  });
-  export default store;
-  
+    user: userSlice.reducer,
+  },
+});
+export default store;
